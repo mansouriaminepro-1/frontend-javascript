6ade97740ef741bbb0d0390517bb4a1315c33097
mansouriaminepro-1 <mansouriamine.pro@gmail.com>
2025-10-19 14:42:26 +0000

    Fix teacher reference in Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -13,7 +13,7 @@ namespace Subjects {
       const teacher = (this as any).teacher as Teacher | undefined;
 
       if (teacher && teacher.experienceTeachingC && teacher.experienceTeachingC > 0) {
-        return `Available Teacher: ${teacher.firstName}`;
+        return "Available Teacher: ${this.teacher.firstName}";
       } else {
         return "No available teacher";
       }
